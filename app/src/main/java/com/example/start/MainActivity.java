1958076b0604b64c611ee0d81e17a120e0040c23
Gabriele <gabriele.sidiskyte@gmail.com>
2025-10-02 00:22:36 +0300

    Teksto keitimas

--- app/src/main/java/com/example/start/MainActivity.java
+++ app/src/main/java/com/example/start/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.start;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tekstas;
+    Button TxtKeitimas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView tekstas = findViewById(R.id.txt);
+        Button TxtKeitimas = findViewById(R.id.btnChangeTxt);
+
+        String[] texts = {"Labas, pasauli!", "Hello World!", "¡Hola, mundo!", "Hallo, Welt!", "Pozdrav, svet!"};
+        final int[] index = {0};
+
+        TxtKeitimas.setOnClickListener(v -> {
+            tekstas.setText(texts[index[0]]);
+            index[0] = (index[0] + 1) % texts.length;
+
         });
     }
 }
